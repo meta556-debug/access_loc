c38e4002a2a3cdbc044e3db049ad900e2d48cffc
meta556-debug <metametia93@gmail.com>
2025-03-19 20:10:02 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,16 +1,1 @@
-<?php
-// Check if lat and long are set in the URL
-if (isset($_GET['lat']) && isset($_GET['long'])) {
-    $lat = $_GET['lat'];
-    $long = $_GET['long'];
 
-    // Prepare the data to be saved, including the current timestamp
-    $data = "Latitude: $lat, Longitude: $long - " . date('Y-m-d H:i:s') . "\n";
-
-    // Specify the path to the .txt file
-    $file = 'locations.txt';
-
-    // Open the file and append the data
-    file_put_contents($file, $data, FILE_APPEND);
-}
-?>
